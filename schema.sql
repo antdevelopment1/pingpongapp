05382b5da0888e14ed0bf7a806cbd0c1d4aa5500
April Copes <antdevelopment1@gmail.com>
2018-11-01 10:14:28 -0400

    Adds create tables and insert

--- schema.sql
+++ schema.sql
@@ -1,1 +1,23 @@
 -- Should should only contain create table statements
+
+create table players (
+    id serial primary key,
+
+    -- Instead of using text we can limit the length of a text field
+    name varchar(100),
+
+);
+
+
+create table games (
+    id serial primary key,
+
+    player1_score integer,
+    player2_score integer,
+
+    winner_id integer references players (id),
+
+    -- These are called "foreign keys"
+    player1_id integer references players (id),
+    player2_id integer references players (id)
+);
--- seed.sql
+++ seed.sql
@@ -1,1 +1,24 @@
 -- Purpose of seed.sql is to fill database with dummy data
+
+insert into players
+    (name) 
+values
+    ('Jeff'),
+    ('Johnny'),
+    ('Sam'),
+    ('John'),
+    ('Trevor'),
+    ('Chris');
+
+
+insert into games
+    (player1_score, player2_score, player1_id, player2_id) 
+values
+    (21, 0, 2, 3),
+    (21, 2, 2, 3),
+    (21, 1, 2, 3),
+    (13, 21, 3,1),
+    (21, 2, 5, 1),
+    (10, 21, 4, 2);
+    
+
